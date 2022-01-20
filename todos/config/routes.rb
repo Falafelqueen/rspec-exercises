ae8065e2397d3ad4fdcb6fd2f9f2b8904e9fd7d8
Lucia Harcegova <fakeluciamail@gmail.com>
2022-01-20 14:54:14 +0100

    created test user can sign in with an email#

--- todos/config/routes.rb
+++ todos/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "todos#index"
+  resources :todos, only: [:index, :new, :create]
+  resource :session, only: [:new, :create]
 
 end
